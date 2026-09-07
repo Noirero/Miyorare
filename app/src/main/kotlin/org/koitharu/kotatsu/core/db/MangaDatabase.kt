f575d1c086fcd9fce7d747900addc90e2d977ddb
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-07 23:48:09 +0700

    Register private favourites schema

--- app/src/main/kotlin/org/koitharu/kotatsu/core/db/MangaDatabase.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/db/MangaDatabase.kt
@@ -55,6 +55,7 @@ import org.koitharu.kotatsu.core.db.migrations.Migration35To36
 import org.koitharu.kotatsu.core.db.migrations.Migration36To37
 import org.koitharu.kotatsu.core.db.migrations.Migration37To38
 import org.koitharu.kotatsu.core.db.migrations.Migration38To39
+import org.koitharu.kotatsu.core.db.migrations.Migration39To40
 import org.koitharu.kotatsu.core.db.migrations.Migration2To3
 import org.koitharu.kotatsu.core.db.migrations.Migration3To4
 import org.koitharu.kotatsu.core.db.migrations.Migration4To5
@@ -68,6 +69,8 @@ import org.koitharu.kotatsu.favourites.data.FavouriteCategoriesDao
 import org.koitharu.kotatsu.favourites.data.FavouriteCategoryEntity
 import org.koitharu.kotatsu.favourites.data.FavouriteEntity
 import org.koitharu.kotatsu.favourites.data.FavouritesDao
+import org.koitharu.kotatsu.favourites.data.PrivateFavouriteEntity
+import org.koitharu.kotatsu.favourites.data.PrivateFavouritesDao
 import org.koitharu.kotatsu.favourites.groups.data.LibraryGroupEntity
 import org.koitharu.kotatsu.favourites.groups.data.LibraryGroupMemberEntity
 import org.koitharu.kotatsu.favourites.groups.data.LibraryGroupsDao
@@ -86,13 +89,13 @@ import org.koitharu.kotatsu.tracker.data.TrackEntity
 import org.koitharu.kotatsu.tracker.data.TrackLogEntity
 import org.koitharu.kotatsu.tracker.data.TracksDao
 
-const val DATABASE_VERSION = 39
+const val DATABASE_VERSION = 40
 
 @Database(
 	entities = [
 		MangaEntity::class, TagEntity::class, HistoryEntity::class, MangaTagsEntity::class, ChapterEntity::class,
-		FavouriteCategoryEntity::class, FavouriteEntity::class, MangaPrefsEntity::class, TrackEntity::class,
-		TrackLogEntity::class, SuggestionEntity::class, BookmarkEntity::class, ScrobblingEntity::class,
+		FavouriteCategoryEntity::class, FavouriteEntity::class, PrivateFavouriteEntity::class, MangaPrefsEntity::class,
+		TrackEntity::class, TrackLogEntity::class, SuggestionEntity::class, BookmarkEntity::class, ScrobblingEntity::class,
 		MangaSourceEntity::class, StatsEntity::class, LocalMangaIndexEntity::class, LibraryGroupEntity::class,
 		LibraryGroupMemberEntity::class, LibraryGroupTimelineItemEntity::class,
 	],
@@ -104,6 +107,7 @@ abstract class MangaDatabase : RoomDatabase() {
 	abstract fun getTagsDao(): TagsDao
 	abstract fun getMangaDao(): MangaDao
 	abstract fun getFavouritesDao(): FavouritesDao
+	abstract fun getPrivateFavouritesDao(): PrivateFavouritesDao
 	abstract fun getPreferencesDao(): PreferencesDao
 	abstract fun getFavouriteCategoriesDao(): FavouriteCategoriesDao
 	abstract fun getTracksDao(): TracksDao
@@ -125,7 +129,7 @@ fun getDatabaseMigrations(context: Context): Array<Migration> = arrayOf(
 	Migration19To20(), Migration20To21(), Migration21To22(), Migration22To23(), Migration23To24(), Migration24To25(),
 	Migration25To26(), Migration26To27(), Migration27To28(), Migration28To29(), Migration29To30(), Migration30To31(),
 	Migration31To32(), Migration32To33(), Migration33To34(), Migration34To35(), Migration35To36(), Migration36To37(),
-	Migration37To38(), Migration38To39(),
+	Migration37To38(), Migration38To39(), Migration39To40(),
 )
 
 fun MangaDatabase(context: Context): MangaDatabase = Room
