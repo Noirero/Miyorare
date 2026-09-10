3e1ce57ede017086d0f039b3010eb012b698e0aa
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-11 03:27:31 +0700

    Opt in download structure bottom sheet API

--- app/src/main/kotlin/org/koitharu/kotatsu/settings/DownloadStructureInfo.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/settings/DownloadStructureInfo.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -92,6 +93,7 @@ internal fun DownloadStructureInfoItem(info: DownloadStructureInfo) {
 	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun DownloadStructureBottomSheet(
 	info: DownloadStructureInfo,
