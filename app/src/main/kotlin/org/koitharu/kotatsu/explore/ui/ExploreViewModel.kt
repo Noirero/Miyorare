18ff90312af5b537ca51999cd81b94f82375a329
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-21 18:01:09 +0700

    Tidy Explore flow imports

--- app/src/main/kotlin/org/koitharu/kotatsu/explore/ui/ExploreViewModel.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/explore/ui/ExploreViewModel.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.plus
 import org.koitharu.kotatsu.R
